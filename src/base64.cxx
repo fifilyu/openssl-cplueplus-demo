b9835beb3e78063c9c27d6a51801711a1f786d45
Fifi Lyu <fifilyu@gmail.com>
2015-04-24 13:33:47 +0800

    增加 Base64 示例代码

---
+++ src/base64.cxx
@@ -0,0 +1,66 @@
+﻿/*
+ * base64.cxx
+ *
+ *  Created on: 2015.4.24
+ *      Author: Fifi Lyu
+ */
+
+#include "base64.h"
+
+size_t plaintext_size(const string &cipher) {
+    const size_t size_ = cipher.size();
+    size_t padding_ = 0;
+
+    if (cipher[size_ - 1] == '=' && cipher[size_ - 2] == '=')
+        padding_ = 2;
+    else if (cipher[size_ - 1] == '=')
+        padding_ = 1;
+    else
+        padding_ = 0;
+
+    return (size_ * 3) / 4 - padding_;
+}
+
+string base64_encode(const string &plaintext) {
+    BIO *bio_ = NULL;
+    BIO *b64_ = NULL;
+    BUF_MEM *bufferPtr_ = NULL;
+
+    b64_ = BIO_new(BIO_f_base64());
+    bio_ = BIO_new(BIO_s_mem());
+    bio_ = BIO_push(b64_, bio_);
+
+    //Ignore newlines - write everything in one line
+    BIO_set_flags(bio_, BIO_FLAGS_BASE64_NO_NL);
+    BIO_write(bio_, plaintext.c_str(), plaintext.size());
+    BIO_flush(bio_);
+    BIO_get_mem_ptr(bio_, &bufferPtr_);
+    BIO_set_close(bio_, BIO_NOCLOSE);
+    BIO_free_all(bio_);
+
+    return string(bufferPtr_->data, bufferPtr_->length);
+}
+
+string base64_decode(const string &cipher) {
+    BIO *bio_ = NULL;
+    BIO *b64_ = NULL;
+
+    const int p_size_ = plaintext_size(cipher.c_str());
+    char buffer[p_size_ + 1];
+
+    bio_ = BIO_new_mem_buf((void*)cipher.c_str(), -1);
+    b64_ = BIO_new(BIO_f_base64());
+    bio_ = BIO_push(b64_, bio_);
+
+    //Do not use newlines to flush buffer
+    BIO_set_flags(bio_, BIO_FLAGS_BASE64_NO_NL);
+    const size_t read_size_ = BIO_read(bio_, buffer, p_size_);
+    BIO_free_all(bio_);
+
+    if (read_size_ == p_size_) {
+        buffer[p_size_] = '\0';
+        return string(buffer);
+    }
+
+    return "";
+}
---
+++ test/base64_unittest.cxx
@@ -0,0 +1,26 @@
+﻿/*
+ * zcmd_unittest.cxx
+ *
+ *  Created on: 2015.3.4
+ *      Author: Fifi Lyu
+ */
+
+#include <gtest/gtest.h>
+#include "../src/base64.h"
+
+// Hello, World! => SGVsbG8sIFdvcmxkIQ==
+const string G_PLAINTEXT("Hello, World!");
+const string G_CIPER("SGVsbG8sIFdvcmxkIQ==");
+
+TEST(Base64Test, plaintext_size) {
+    const string ciper_(G_CIPER);
+    EXPECT_EQ(G_PLAINTEXT.size(), plaintext_size(ciper_));
+}
+
+TEST(Base64Test, base64_encode) {
+    EXPECT_STREQ(G_CIPER.c_str(), base64_encode(G_PLAINTEXT).c_str());
+}
+
+TEST(Base64Test, base64_decode) {
+    EXPECT_STREQ(G_PLAINTEXT.c_str(), base64_decode(G_CIPER).c_str());
+}
